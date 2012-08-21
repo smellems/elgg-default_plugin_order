dd4c7a4bff592561e4f8e0256e41b3d1ed54a075
Lowfill Tarmak <lowfill@lowfill.org>
2012-08-21 13:45:44 -0700

    Fixed manifest file added 'cache' management. Added example config.ini file

--- start.php
+++ start.php
@@ -16,38 +16,47 @@ function elgg_default_plugin_order_load_config(){
 	   //TODO Add sussport for sections to handle diferent environments
 	   $config_settings = parse_ini_file($config_file);
 	}
-	// TODO Hash configurations for execute this only when necessary
-	return $config_settings;	
+	$config_hash = md5(serialize($config_settings));
+	$old_config = get_plugin_setting('config','elgg_default_plugin_order');
+	if($old_config != $config_hash){
+		set_plugin_setting('config',$config_hash,'elgg_default_plugin_order');
+		return $config_settings;
+	}
+	return array();	
 }
 function elgg_default_plugin_order_reorder(){
 	$final_order = array();
-	$secuence = 10;
+	$sequence = 10;
 	$config_settings = elgg_default_plugin_order_load_config();
-	foreach($config_settings as $plugin => $status){
-		$final_order[$sequence] = $plugin;
-		$secuence+=10;
-	}	
-	regenerate_plugin_list($final_order);
-	elgg_filepath_cache_reset();
+	if(!empty($config_settings)){
+		foreach($config_settings as $plugin => $status){
+			$final_order[$sequence] = $plugin;
+			$sequence+=10;
+		}
+		regenerate_plugin_list($final_order);
+		elgg_filepath_cache_reset();
+	}
 	
 }
 function elgg_default_plugin_order_set_status(){
 	$config_settings = elgg_default_plugin_order_load_config();
-	foreach($config_settings as $plugin => $status){
-		switch($status){
-			case 'enabled':
-				if(!is_plugin_enabled($plugin)){
-					enable_plugin($plugin);
-				}
-			break;
-			case 'disabled':
-				if(is_plugin_enabled($plugin)){
-					disable_plugin($plugin);
-				}
-			break;
+	if(!empty($config_settings)){
+		foreach($config_settings as $plugin => $status){
+			switch($status){
+				case 'enabled':
+					if(!is_plugin_enabled($plugin)){
+						enable_plugin($plugin);
+					}
+				break;
+				case 'disabled':
+					if(is_plugin_enabled($plugin)){
+						disable_plugin($plugin);
+					}
+				break;
+			}
 		}
+		elgg_filepath_cache_reset();
 	}
-	elgg_filepath_cache_reset();
 }
 
 
